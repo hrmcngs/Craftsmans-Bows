6fd1a25c254a8d705515809854a14b6a769196cb
0526ngs <78349683+hrmcngs@users.noreply.github.com>
2025-03-18 21:26:15 +0900

    Update item.java

--- remappedSrc/com/craftsman_bows/init/item.java
+++ remappedSrc/com/craftsman_bows/init/item.java
@@ -1,52 +1,41 @@
 package com.craftsman_bows.init;
 
 import com.craftsman_bows.item.*;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.*;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.util.Identifier;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.event.CreativeModeTabEvent.BuildContents;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 
 import static com.craftsman_bows.CraftsmanBows.Mod_ID;
-import static net.minecraft.item.Items.NETHERITE_INGOT;
-import static net.minecraft.registry.tag.ItemTags.PLANKS;
+import static net.minecraft.world.item.Items.NETHERITE_INGOT;
 
-public class item {
+@Mod.EventBusSubscriber(modid = Mod_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModItems {
 
-    public static Item register(String id, Item item) {
-        // アイテムの識別子を作成
-        Identifier itemID = Identifier.of(Mod_ID, id);
+    public static final Item SHORT_BOW = register("shortbow", new ShortBowItem(new Item.Properties().stacksTo(1).durability(576)));
+    public static final Item LONG_BOW = register("longbow", new LongBowItem(new Item.Properties().stacksTo(1).durability(576)));
+    public static final Item SHOT_CROSSBOW = register("shot_crossbow", new ShotCrossbowItem(new Item.Properties().stacksTo(1).durability(576)));
+    public static final Item REPEATER_CROSSBOW = register("repeater_crossbow", new RepeaterCrossbowItem(new Item.Properties().stacksTo(1).durability(640).fireResistant()));
+    public static final Item BURST_ARBALEST = register("burst_arbalest", new BurstArbalestItem(new Item.Properties().stacksTo(1).durability(900).fireResistant()));
 
-        // アイテムを登録し、登録したアイテムを返す
-        return Registry.register(Registries.ITEM, itemID, item);
+    private static Item register(String id, Item item) {
+        return Registry.register(Registry.ITEM, new ResourceLocation(Mod_ID, id), item);
     }
 
-    public static Item.Settings createSettings(String id) {
-        Identifier itemID = Identifier.of(Mod_ID, id);
-        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, itemID);
-
-        // 新しいItem.Settingsを生成して、registryKeyを設定
-        return new Item.Settings().registryKey(itemKey);
-    }
-
-    // 各アイテムを異なるクラスで生成し、設定済みで登録
-    public static final Item SHORT_BOW = register("shortbow", new ShortBowItem(createSettings("shortbow").maxCount(1).maxDamage(576).enchantable(1).repairable(PLANKS)));
-    public static final Item LONG_BOW = register("longbow", new LongBowItem(createSettings("longbow").maxCount(1).maxDamage(576).enchantable(1).repairable(PLANKS)));
-    public static final Item SHOT_CROSSBOW = register("shot_crossbow", new ShotCrossbowItem(createSettings("shot_crossbow").maxCount(1).maxDamage(576).enchantable(1).repairable(PLANKS)));
-    public static final Item REPEATER_CROSSBOW = register("repeater_crossbow", new RepeaterCrossbowItem(createSettings("repeater_crossbow").maxCount(1).maxDamage(640).enchantable(1).fireproof().repairable(NETHERITE_INGOT)));
-    public static final Item BURST_ARBALEST = register("burst_arbalest", new BurstArbalestItem(createSettings("burst_arbalest").maxCount(1).maxDamage(900).enchantable(1).fireproof().repairable(NETHERITE_INGOT)));
-
-    public static void init() {
-
-        // アイテムのグループを武器に
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(content -> {
-            content.add(SHORT_BOW);
-            content.add(LONG_BOW);
-            content.add(SHOT_CROSSBOW);
-            content.add(REPEATER_CROSSBOW);
-            content.add(BURST_ARBALEST);
-        });
+    @SubscribeEvent
+    public static void onCreativeModeTabBuildContents(BuildContents event) {
+        if (event.getTab() == CreativeModeTabs.TAB_COMBAT) {
+            event.accept(new ItemStack(SHORT_BOW));
+            event.accept(new ItemStack(LONG_BOW));
+            event.accept(new ItemStack(SHOT_CROSSBOW));
+            event.accept(new ItemStack(REPEATER_CROSSBOW));
+            event.accept(new ItemStack(BURST_ARBALEST));
+        }
     }
 }
