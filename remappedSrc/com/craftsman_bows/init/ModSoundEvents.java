593d9c85f06f28620bc8f2e033a76195b0738454
0526ngs <78349683+hrmcngs@users.noreply.github.com>
2025-03-18 21:25:04 +0900

    Update ModSoundEvents.java

--- remappedSrc/com/craftsman_bows/init/ModSoundEvents.java
+++ remappedSrc/com/craftsman_bows/init/ModSoundEvents.java
@@ -1,31 +1,33 @@
 package com.craftsman_bows.init;
 
 import com.craftsman_bows.CraftsmanBows;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvent;
 
 public class ModSoundEvents {
 
-    public static final net.minecraft.sound.SoundEvent LEGACY_BOW_SHOOT_1 = registerSoundEvent("legacy_arrow_1");
-    public static final net.minecraft.sound.SoundEvent LEGACY_BOW_SHOOT_2 = registerSoundEvent("legacy_arrow_2");
+    public static final SoundEvent LEGACY_BOW_SHOOT_1 = registerSoundEvent("legacy_arrow_1");
+    public static final SoundEvent LEGACY_BOW_SHOOT_2 = registerSoundEvent("legacy_arrow_2");
 
-    public static final net.minecraft.sound.SoundEvent DUNGEONS_BOW_CHARGE_1 = registerSoundEvent("dungeons_bow_charge_1");
-    public static final net.minecraft.sound.SoundEvent DUNGEONS_BOW_CHARGE_2 = registerSoundEvent("dungeons_bow_charge_2");
-    public static final net.minecraft.sound.SoundEvent DUNGEONS_BOW_CHARGE_3 = registerSoundEvent("dungeons_bow_charge_3");
-    public static final net.minecraft.sound.SoundEvent DUNGEONS_BOW_CHARGE_4 = registerSoundEvent("dungeons_bow_charge_4");
+    public static final SoundEvent DUNGEONS_BOW_CHARGE_1 = registerSoundEvent("dungeons_bow_charge_1");
+    public static final SoundEvent DUNGEONS_BOW_CHARGE_2 = registerSoundEvent("dungeons_bow_charge_2");
+    public static final SoundEvent DUNGEONS_BOW_CHARGE_3 = registerSoundEvent("dungeons_bow_charge_3");
+    public static final SoundEvent DUNGEONS_BOW_CHARGE_4 = registerSoundEvent("dungeons_bow_charge_4");
 
-    public static final net.minecraft.sound.SoundEvent DUNGEONS_BOW_LOAD = registerSoundEvent("dungeons_bow_load");
-    public static final net.minecraft.sound.SoundEvent DUNGEONS_BOW_SHOOT = registerSoundEvent("dungeons_bow_shoot");
-    public static final net.minecraft.sound.SoundEvent DUNGEONS_COG_CROSSBOW_PICKUP = registerSoundEvent("dungeons_cog_crossbow_pickup");
-    public static final net.minecraft.sound.SoundEvent DUNGEONS_COG_CROSSBOW_SHOOT = registerSoundEvent("dungeons_cog_crossbow_shoot");
-    public static final net.minecraft.sound.SoundEvent DUNGEONS_COG_CROSSBOW_PLACE = registerSoundEvent("dungeons_cog_crossbow_place");
+    public static final SoundEvent DUNGEONS_BOW_LOAD = registerSoundEvent("dungeons_bow_load");
+    public static final SoundEvent DUNGEONS_BOW_SHOOT = registerSoundEvent("dungeons_bow_shoot");
+    public static final SoundEvent DUNGEONS_COG_CROSSBOW_PICKUP = registerSoundEvent("dungeons_cog_crossbow_pickup");
+    public static final SoundEvent DUNGEONS_COG_CROSSBOW_SHOOT = registerSoundEvent("dungeons_cog_crossbow_shoot");
+    public static final SoundEvent DUNGEONS_COG_CROSSBOW_PLACE = registerSoundEvent("dungeons_cog_crossbow_place");
 
-    private static net.minecraft.sound.SoundEvent registerSoundEvent(String name) {
-        Identifier id = Identifier.of(CraftsmanBows.Mod_ID, name);
-        return Registry.register(Registries.SOUND_EVENT, id, net.minecraft.sound.SoundEvent.of(id));
+    private static SoundEvent registerSoundEvent(String name) {
+        ResourceLocation id = new ResourceLocation(CraftsmanBows.Mod_ID, name);
+        return Registry.register(Registry.SOUND_EVENT, id, new SoundEvent(id));
     }
 
-        public static void init() {
+    public static void init() {
+        // コンソールにMODの初期化が完了したことを通知
+        System.out.println("ModSoundEvents initialized");
     }
 }
