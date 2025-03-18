8a4104ab1e950bd7d656cf7308cd020a697b6407
0526ngs <78349683+hrmcngs@users.noreply.github.com>
2025-03-18 21:48:11 +0900

    Update ShortBowItem.java

--- remappedSrc/com/craftsman_bows/item/ShortBowItem.java
+++ remappedSrc/com/craftsman_bows/item/ShortBowItem.java
@@ -1,22 +1,24 @@
 package com.craftsman_bows.item;
 
 import com.craftsman_bows.interfaces.item.CanSprintWhileUsing;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.*;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.Hand;
-import net.minecraft.util.ActionResult;
-import net.minecraft.world.World;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BowItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.level.Level;
 import com.craftsman_bows.init.ModSoundEvents;
 
 import java.util.List;
 
 public class ShortBowItem extends CraftsmanBowItem implements CanSprintWhileUsing {
-    public ShortBowItem(net.minecraft.item.Item.Settings settings) {
-        super(settings);
+    public ShortBowItem(Item.Properties properties) {
+        super(properties);
     }
 
     // 弓を引いた時間を取得する処理のようだ。今回は書き換えて、0.55以上引き絞ったら強制的に1（フルチャージ）になるようにした
@@ -30,74 +32,72 @@ public class ShortBowItem extends CraftsmanBowItem implements CanSprintWhileUsin
 
     // アイテムを使用しているときの処理
     @Override
-    public void usageTick(World world, LivingEntity user, ItemStack stack, int remainingUseTicks) {
-        int i = this.getMaxUseTime(stack, user) - remainingUseTicks;
+    public void onUseTick(Level level, LivingEntity user, ItemStack stack, int remainingUseTicks) {
+        int i = this.getUseDuration(stack) - remainingUseTicks;
 
         // チャージ中
         if (i < 10) {
-            chargingParticle(world, user);
+            chargingParticle(level, user);
         }
 
         // チャージ完了
         if (i == 10) {
-            chargeEndParticle(world, user);
-            user.playSound(ModSoundEvents.DUNGEONS_BOW_CHARGE_1, 1.0f, 1.4f );
+            chargeEndParticle(level, user);
+            user.playSound(ModSoundEvents.DUNGEONS_BOW_CHARGE_1, 1.0f, 1.4f);
         }
     }
 
     // 最初の使用時のアクション
     @Override
-    public ActionResult use(World world, PlayerEntity user, Hand hand) {
-        ItemStack itemStack = user.getStackInHand(hand);
-        boolean bl = !user.getProjectileType(itemStack).isEmpty();
-        if (!user.isInCreativeMode() && !bl) {
-            return ActionResult.FAIL;
+    public InteractionResult use(Level level, Player user, InteractionHand hand) {
+        ItemStack itemStack = user.getItemInHand(hand);
+        boolean bl = !user.getProjectile(itemStack).isEmpty();
+        if (!user.getAbilities().instabuild && !bl) {
+            return InteractionResult.FAIL;
         } else {
             user.playSound(ModSoundEvents.DUNGEONS_BOW_LOAD, 1.0f, 1.2f);
-            user.setCurrentHand(hand);
-            return ActionResult.CONSUME;
+            user.startUsingItem(hand);
+            return InteractionResult.CONSUME;
         }
     }
 
     // 使用をやめたとき、つまりクリックを離したときの処理だ。
     @Override
-    public boolean onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks) {
-
-        if (!(user instanceof PlayerEntity playerEntity)) {
-            return false;
+    public void releaseUsing(ItemStack stack, Level level, LivingEntity user, int remainingUseTicks) {
+        if (!(user instanceof Player player)) {
+            return;
         }
 
         // プレイヤーを定義する処理のようだ。後は…手持ちの矢の種類を取得する処理？
-        ItemStack itemStack = playerEntity.getProjectileType(stack);
+        ItemStack itemStack = player.getProjectile(stack);
         if (itemStack.isEmpty()) {
-            return false;
+            return;
         }
 
         // 使用時間0.1未満では使用をキャンセルする処理のようだ
-        int i = this.getMaxUseTime(stack, user) - remainingUseTicks;
+        int i = this.getUseDuration(stack) - remainingUseTicks;
         float f = getPullProgress(i);
         if ((double) f < 0.1) {
-            return false;
+            return;
         }
 
         // パーティクル
         if (f >= 1) {
-            shootParticle(world, user);
+            shootParticle(level, user);
         }
 
         // ここが放つ処理に見える。
-        List<ItemStack> list = BowItem.load(stack, itemStack, playerEntity);
-        if (world instanceof ServerWorld serverWorld) {
+        List<ItemStack> list = BowItem.getChargedProjectiles(stack);
+        if (level instanceof ServerLevel serverLevel) {
             if (!list.isEmpty()) {
-                this.shootAll(serverWorld, playerEntity, playerEntity.getActiveHand(), stack, list, f * 1.6f, 1.0f, f == 1.0f, null);
+                this.performShooting(serverLevel, player, player.getUsedItemHand(), stack, list, f * 1.6f, 1.0f, f == 1.0f);
             }
             if (f < 1) {
-                world.playSound(null, playerEntity.getX(), playerEntity.getY(), playerEntity.getZ(), SoundEvents.ENTITY_ARROW_SHOOT, SoundCategory.PLAYERS, 1.0f, 1.0f / (world.getRandom().nextFloat() * 0.4f + 1.2f) + f * 0.5f);
+                level.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ARROW_SHOOT, SoundSource.PLAYERS, 1.0f, 1.0f / (level.getRandom().nextFloat() * 0.4f + 1.2f) + f * 0.5f);
             } else {
-                world.playSound(null, playerEntity.getX(), playerEntity.getY(), playerEntity.getZ(), ModSoundEvents.LEGACY_BOW_SHOOT_1, SoundCategory.PLAYERS, 1.0f, 0.8f / (world.getRandom().nextFloat() * 0.4f + 1.2f) + 0.9f);
-                world.playSound(null, playerEntity.getX(), playerEntity.getY(), playerEntity.getZ(), ModSoundEvents.DUNGEONS_BOW_SHOOT, SoundCategory.PLAYERS, 1.0f, 1.4f);
+                level.playSound(null, player.getX(), player.getY(), player.getZ(), ModSoundEvents.LEGACY_BOW_SHOOT_1, SoundSource.PLAYERS, 1.0f, 0.8f / (level.getRandom().nextFloat() * 0.4f + 1.2f));
+                level.playSound(null, player.getX(), player.getY(), player.getZ(), ModSoundEvents.DUNGEONS_BOW_SHOOT, SoundSource.PLAYERS, 1.0f, 1.4f);
             }
         }
-        return true;
     }
 }
