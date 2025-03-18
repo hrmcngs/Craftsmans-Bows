9fbbd051f02287db96bf5a16cd52dfc899b422fa
0526ngs <78349683+hrmcngs@users.noreply.github.com>
2025-03-18 23:35:16 +0900

    Update CraftsmanBows.java

--- remappedSrc/com/craftsman_bows/CraftsmanBows.java
+++ remappedSrc/com/craftsman_bows/CraftsmanBows.java
@@ -3,15 +3,14 @@ package com.craftsman_bows;
 import com.craftsman_bows.init.*;
 import net.fabricmc.api.ModInitializer;
 
-
 public class CraftsmanBows implements ModInitializer {
-	public static final String Mod_ID = "craftsman_bows";
+    public static final String Mod_ID = "craftsman_bows";
 
-	@Override
-	public void onInitialize() {
-		item.init();
-		ModParticleTypes.init();
-		ModSoundEvents.init();
-		ModComponents.init();
-	}
+    @Override
+    public void onInitialize() {
+        item.init();
+        ModParticleTypes.init();
+        ModSoundEvents.init();
+        ModComponents.init();
+    }
 }
