9eead4f4266b28a2fcf6ad5b6b0842d3f8b4e01e
0526ngs <78349683+hrmcngs@users.noreply.github.com>
2025-03-19 14:28:57 +0900

    Update ModParticleTypes.java

--- src/main/java/com/craftsman_bows/init/ModParticleTypes.java
+++ src/main/java/com/craftsman_bows/init/ModParticleTypes.java
@@ -1,22 +1,35 @@
 package com.craftsman_bows.init;
 
-import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
-import net.minecraft.particle.SimpleParticleType;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
-
+import net.minecraft.particles.ParticleType;
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import static com.craftsman_bows.CraftsmanBows.Mod_ID;
 
+@Mod.EventBusSubscriber(modid = Mod_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModParticleTypes {
 
-    public static final SimpleParticleType CHARGE_DUST = FabricParticleTypes.simple();
-    public static final SimpleParticleType CHARGE_END = FabricParticleTypes.simple();
-    public static final SimpleParticleType SHOOT = FabricParticleTypes.simple();
+    public static final DeferredRegister<ParticleType<?>> PARTICLES = DeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, Mod_ID);
 
-    public static void init() {
-        Registry.register(Registries.PARTICLE_TYPE, Identifier.of(Mod_ID, "charge_dust"), CHARGE_DUST);
-        Registry.register(Registries.PARTICLE_TYPE, Identifier.of(Mod_ID, "charge_end"), CHARGE_END);
-        Registry.register(Registries.PARTICLE_TYPE, Identifier.of(Mod_ID, "shoot"), SHOOT);
+    @ObjectHolder(Mod_ID + ":charge_dust")
+    public static final RegistryObject<ParticleType<?>> CHARGE_DUST = PARTICLES.register("charge_dust", () -> new ParticleType<>(false, ParticleType.Deserializer));
+
+    @ObjectHolder(Mod_ID + ":charge_end")
+    public static final RegistryObject<ParticleType<?>> CHARGE_END = PARTICLES.register("charge_end", () -> new ParticleType<>(false, ParticleType.Deserializer));
+
+    @ObjectHolder(Mod_ID + ":shoot")
+    public static final RegistryObject<ParticleType<?>> SHOOT = PARTICLES.register("shoot", () -> new ParticleType<>(false, ParticleType.Deserializer));
+
+    @SubscribeEvent
+    public static void registerParticles(final RegistryEvent.Register<ParticleType<?>> event) {
+        event.getRegistry().registerAll(
+            CHARGE_DUST.get(),
+            CHARGE_END.get(),
+            SHOOT.get()
+        );
     }
 }
