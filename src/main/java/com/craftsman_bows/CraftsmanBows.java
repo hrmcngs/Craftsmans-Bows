91806ffa5c3a9d508824ce7c92edb135463239aa
0526ngs <78349683+hrmcngs@users.noreply.github.com>
2025-03-19 19:24:44 +0900

    Update CraftsmanBows.java

--- src/main/java/com/craftsman_bows/CraftsmanBows.java
+++ src/main/java/com/craftsman_bows/CraftsmanBows.java
@@ -1,20 +1,14 @@
 package com.craftsman_bows;
 
 import com.craftsman_bows.init.*;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(CraftsmanBows.Mod_ID)
 public class CraftsmanBows {
     public static final String Mod_ID = "craftsman_bows";
 
-    public CraftsmanBows() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        
-        item.ITEMS.register(modEventBus);
-        ModParticleTypes.PARTICLES.register(modEventBus);
-        ModSoundEvents.SOUNDS.register(modEventBus);
-        ModComponents.COMPONENTS.register(modEventBus);
+    public static void onInitialize() {
+        item.init();
+        ModParticleTypes.init();
+        ModSoundEvents.init();
+        ModComponents.init();
     }
 }
